77ee324e32ec86596d539b4970bc266b88f36803
Mawuli Agbeko <jmmyagbeko@st.ug.edu.gh>
2024-05-17 07:13:39 -1200

    Task 2 added

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,17 +1,42 @@
-// TASK 1
 function processArray(numbers) {
-  var processArray = [];
+  var processedArray = [];
   for (var i = 0; i < numbers.length; i++) {
     var number = numbers[i];
 
     if (number % 2 === 0) {
-      processArray.push(number ** 2);
+      processedArray.push(number ** 2);
     } else {
-      processArray.push(number * 3);
+      processedArray.push(number * 3);
     }
   }
-  return processArray;
+  return processedArray;
 }
-let numbers = [31, 14, 11, 23, 20];
-let output = processArray(numbers);
+
+const numbers = [31, 14, 11, 23, 20];
+var output = processArray(numbers);
 console.log(output);
+
+// Task 2
+function formatArrayStrings(strings, numbers) {
+  const formattedStrings = [];
+
+  for (let i = 0; i < strings.length; i++) {
+    const string = strings[i];
+    const number = numbers[i];
+
+    if (number % 2 === 0) {
+      formattedStrings.push(string.toUpperCase());
+    } else {
+      formattedStrings.push(string.toLowerCase());
+    }
+  }
+
+  return formattedStrings;
+}
+
+const strings = ["covert", "neji", "kage", "gouls", "juke"];
+const num = [31, 14, 11, 23, 20];
+
+const processedNumbers = processArray(num);
+const formatted = formatArrayStrings(strings, processedNumbers);
+console.log(formatted);
