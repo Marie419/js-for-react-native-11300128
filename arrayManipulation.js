5c28547388fb829e2a6d09e181c2050b79ae368e
Mawuli Agbeko <jmmyagbeko@st.ug.edu.gh>
2024-05-19 03:37:44 -1200

    added task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -15,3 +15,28 @@ function processArray(numbers) {
 let numbers = [31, 14, 11, 23, 20];
 let output = processArray(numbers);
 console.log(output);
+
+// Task 2
+function formatArrayStrings(strings, numbers) {
+  const formattedStrings = [];
+
+  for (let i = 0; i < strings.length; i++) {
+    const string = strings[i];
+    const number = numbers[i];
+
+    if (number % 2 === 0) {
+      formattedStrings.push(string.toUpperCase());
+    } else {
+      formattedStrings.push(string.toLowerCase());
+    }
+  }
+
+  return formattedStrings;
+}
+
+const strings = ["covert", "neji", "kage", "gouls", "juke"];
+const num = [31, 14, 11, 23, 20];
+
+const processedNumbers = processArray(num);
+const formatted = formatArrayStrings(strings, processedNumbers);
+console.log(formatted);
